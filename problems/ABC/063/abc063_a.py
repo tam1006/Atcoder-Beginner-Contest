a5a0ea6766f4fdb74530215a05b10165cc234abe
tam1006 <tamusyun1006@gmail.com>
2022-05-11 23:24:31 +0900

    abc 063

---
+++ problems/ABC/063/abc063_a.py
@@ -0,0 +1,7 @@
+A, B = map(int, input().split())
+
+ans = A+B
+if ans >= 10:
+    print('error')
+else:
+    print(ans)
---
+++ problems/ABC/063/abc063_b.py
@@ -0,0 +1,6 @@
+S = input()
+
+if len(list(set(S))) == len(S):
+    print('yes')
+else:
+    print('no')
