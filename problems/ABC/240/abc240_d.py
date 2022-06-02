03e4e86fea39b3d08451aec7a210ee67d12d9755
tam1006 <tamusyun1006@gmail.com>
2022-06-03 01:43:46 +0900

    abc 240

--- problems/ABC/240/abc240_d.py
+++ problems/ABC/240/abc240_d.py
@@ -1,11 +1,23 @@
+from collections import deque
 N = int(input())
-a = list(map(int, input().split()))
+a = deque(list(map(int, input().split())))
 
-res = []
-for i in range(N):
-    res.append(a[i])
-    # print(res[-a[i]:])
-    if len(res) >= a[i] and res[-a[i]:] == [a[i]]*a[i]:
-        res = res[:-a[i]]
-    print(len(res))
-    # print(res)
+ans = 0
+ans_lis = deque()
+while a:
+    b = a.popleft()
+    if len(ans_lis) == 0:
+        ans_lis.append([b, 1])
+        ans += 1
+    elif b == ans_lis[-1][0]:
+        ans_lis[-1][1] += 1
+        if ans_lis[-1][1] == ans_lis[-1][0]:
+            ans -= ans_lis[-1][0] - 1
+            ans_lis.pop()
+        else:
+            ans += 1
+    else: 
+        ans_lis.append([b, 1])
+        ans += 1
+        
+    print(ans)
