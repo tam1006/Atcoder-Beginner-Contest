b148a48f953e78d65acd1ab6ff9d2e99c6d794ee
tam1006 <tamusyun1006@gmail.com>
2022-07-07 01:27:12 +0900

    abc 177

---
+++ problems/ABC/177/abc177_c.py
@@ -0,0 +1,13 @@
+N = int(input())
+A = list(map(int, input().split()))
+mod = 10**9 + 7
+
+s = sum(A)
+ans = 0
+
+for i in range(N):
+    s -= A[i]
+    ans += (A[i]*s)
+    ans %= mod
+
+print(int(ans))
