f686ecd6c4ce9d74d4cf6ff83da792a98d62210f
tam1006 <tamusyun1006@gmail.com>
2022-07-17 19:17:43 +0900

    abc 167

---
+++ tips/bit.py
@@ -0,0 +1,12 @@
+money = 300
+item = (("みかん", 100), ("りんご", 200), ("ぶどう", 300))
+n = len(item)
+for i in range(2 ** n):
+    bag = []
+    total = 0
+    for j in range(n):  # このループが一番のポイント
+        if ((i >> j) & 1):  # 順に右にシフトさせ最下位bitのチェックを行う
+            bag.append(item[j][0])  # フラグが立っていたら bag に果物を詰める
+            total += item[j][1]  # 買い物累計額にも加算
+    if (total <= money):
+        print(total, bag)
