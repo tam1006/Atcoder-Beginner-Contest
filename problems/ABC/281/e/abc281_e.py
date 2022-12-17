1aae9cc3020ada2de9a0f22227483367ad02b82a
tam1006 <tamusyun1006@gmail.com>
2022-12-17 20:52:35 +0900

    change abc 281 e

--- problems/ABC/281/e/abc281_e.py
+++ problems/ABC/281/e/abc281_e.py
@@ -1,10 +1,179 @@
 #!/usr/bin/env python3
 # from typing import *
 
+# https://github.com/tatyam-prime/SortedSet/blob/main/SortedMultiset.py
+import math
+from bisect import bisect_left, bisect_right, insort
+from typing import Generic, Iterable, Iterator, TypeVar, Union, List
+T = TypeVar('T')
+
+class SortedMultiset(Generic[T]):
+    """
+    Sorted Set の多重集合版。
+    同じ要素を複数持つことができる。
+    """
+    BUCKET_RATIO = 50
+    REBUILD_RATIO = 170
+
+    def _build(self, a=None) -> None:
+        "Evenly divide `a` into buckets."
+        if a is None: a = list(self)
+        size = self.size = len(a)
+        bucket_size = int(math.ceil(math.sqrt(size / self.BUCKET_RATIO)))
+        self.a = [a[size * i // bucket_size : size * (i + 1) // bucket_size] for i in range(bucket_size)]
+    
+    def __init__(self, a: Iterable[T] = []) -> None:
+        "Make a new SortedMultiset from iterable. / O(N) if sorted / O(N log N)"
+        a = list(a)
+        if not all(a[i] <= a[i + 1] for i in range(len(a) - 1)):
+            a = sorted(a)
+        self._build(a)
+
+    def __iter__(self) -> Iterator[T]:
+        for i in self.a:
+            for j in i: yield j
+
+    def __reversed__(self) -> Iterator[T]:
+        for i in reversed(self.a):
+            for j in reversed(i): yield j
+    
+    def __len__(self) -> int:
+        return self.size
+    
+    def __repr__(self) -> str:
+        return "SortedMultiset" + str(self.a)
+    
+    def __str__(self) -> str:
+        s = str(list(self))
+        return "{" + s[1 : len(s) - 1] + "}"
+
+    def _find_bucket(self, x: T) -> List[T]:
+        "Find the bucket which should contain x. self must not be empty."
+        for a in self.a:
+            if x <= a[-1]: return a
+        return a
+
+    def __contains__(self, x: T) -> bool:
+        if self.size == 0: return False
+        a = self._find_bucket(x)
+        i = bisect_left(a, x)
+        return i != len(a) and a[i] == x
+
+    def count(self, x: T) -> int:
+        "Count the number of x."
+        return self.index_right(x) - self.index(x)
+
+    def add(self, x: T) -> None:
+        "Add an element. / O(√N)"
+        if self.size == 0:
+            self.a = [[x]]
+            self.size = 1
+            return
+        a = self._find_bucket(x)
+        insort(a, x)
+        self.size += 1
+        if len(a) > len(self.a) * self.REBUILD_RATIO:
+            self._build()
+
+    def discard(self, x: T) -> bool:
+        "Remove an element and return True if removed. / O(√N)"
+        if self.size == 0: return False
+        a = self._find_bucket(x)
+        i = bisect_left(a, x)
+        if i == len(a) or a[i] != x: return False
+        a.pop(i)
+        self.size -= 1
+        if len(a) == 0: self._build()
+        return True
+
+    def lt(self, x: T) -> Union[T, None]:
+        "Find the largest element < x, or None if it doesn't exist."
+        for a in reversed(self.a):
+            if a[0] < x:
+                return a[bisect_left(a, x) - 1]
+
+    def le(self, x: T) -> Union[T, None]:
+        "Find the largest element <= x, or None if it doesn't exist."
+        for a in reversed(self.a):
+            if a[0] <= x:
+                return a[bisect_right(a, x) - 1]
+
+    def gt(self, x: T) -> Union[T, None]:
+        "Find the smallest element > x, or None if it doesn't exist."
+        for a in self.a:
+            if a[-1] > x:
+                return a[bisect_right(a, x)]
+
+    def ge(self, x: T) -> Union[T, None]:
+        "Find the smallest element >= x, or None if it doesn't exist."
+        for a in self.a:
+            if a[-1] >= x:
+                return a[bisect_left(a, x)]
+    
+    def __getitem__(self, x: int) -> T:
+        "Return the x-th element, or IndexError if it doesn't exist."
+        if x < 0: x += self.size
+        if x < 0: raise IndexError
+        for a in self.a:
+            if x < len(a): return a[x]
+            x -= len(a)
+        raise IndexError
+
+    def index(self, x: T) -> int:
+        "Count the number of elements < x."
+        ans = 0
+        for a in self.a:
+            if a[-1] >= x:
+                return ans + bisect_left(a, x)
+            ans += len(a)
+        return ans
+
+    def index_right(self, x: T) -> int:
+        "Count the number of elements <= x."
+        ans = 0
+        for a in self.a:
+            if a[-1] > x:
+                return ans + bisect_right(a, x)
+            ans += len(a)
+        return ans
 
 # def solve(N: int, M: int, K: int, A: List[int]) -> List[str]:
 def solve(N, M, K, A):
-    pass  # TODO: edit here
+    tmp = A[:M]
+    tmp.sort()
+    L = SortedMultiset(tmp[:K])
+    if M == K:
+        R = SortedMultiset([float('inf')])
+    else:
+        R = SortedMultiset(tmp[K:])
+    ans = [sum(tmp[:K])]
+
+    for i in range(N-M):
+        if A[i] in L:
+            L.discard(A[i])
+            if A[i+M] < R[0]:
+                L.add(A[i+M])
+                ans.append(ans[-1] - A[i] + A[i+M])
+            else:
+                r = R[0]
+                L.add(r)
+                R.discard(r)
+                R.add(A[i+M])
+                ans.append(ans[-1] - A[i] + r)
+        else:
+            R.discard(A[i])
+            if A[i+M] > L[K-1]:
+                R.add(A[i+M])
+                ans.append(ans[-1])
+            else:
+                l = L[K-1]
+                R.add(l)
+                L.discard(l)
+                L.add(A[i+M])
+                ans.append(ans[-1] - l + A[i+M])
+
+    return ans
+
 
 
 # generated by oj-template v4.8.1 (https://github.com/online-judge-tools/template-generator)
@@ -19,7 +188,7 @@ def main():
         A[i] = int(next(tokens))
     assert next(tokens, None) is None
     a = solve(N, M, K, A)
-    print(*[a[i] for i in range(M)])
+    print(*a)
 
 
 if __name__ == '__main__':
