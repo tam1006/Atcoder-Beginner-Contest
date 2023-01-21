e26403d1dbe288387a20b2b10aee55d4811107bc
tam1006 <tamusyun1006@gmail.com>
2023-01-21 22:01:46 +0900

    add ABC 062

---
+++ problems/ABC/062/c/abc062_c.py
@@ -0,0 +1,11 @@
+def solve(H, W):
+    if H % 3 == 0 or W % 3 == 0:
+        return 0
+    if 
+
+def main():
+    H, W = map(int, input().split())
+    print(solve(H, W))
+
+if __name__ == '__main__':
+    main()
