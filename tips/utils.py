5d85a0e7014cb2f5abf96585dcfd98185652ca5d
tam1006 <tamusyun1006@gmail.com>
2023-02-18 20:09:19 +0900

    add nCr

--- tips/utils.py
+++ tips/utils.py
@@ -1,4 +1,4 @@
-MOD = 998244353
+MOD = pow(10, 9) + 7
 
 def dot(A, B):
     Ah, Bh, Bw = len(A), len(B), len(B[0])
@@ -36,3 +36,27 @@ def list_primes(limit):
             is_prime[i] = False
 
     return primes
+
+
+
+# cmb mod M の、逆元を用いた高速化
+# https://blog.satoooh.com/entry/5195/
+fact = [1, 1]
+factinv = [1, 1]
+inv = [0, 1]
+
+N = pow(10, 6)
+
+for i in range(2, N+1):
+    fact.append((fact[-1] * i) % MOD)
+    inv.append((-inv[MOD%i] * (MOD // i) % MOD))
+    factinv.append(((factinv[-1] * inv[-1]) % MOD))
+
+def cmb(n, r, p):
+    if (r < 0) or n < r:
+        return 0
+
+    r = min(r, n-r)
+    return fact[n] * factinv[r] * factinv[n-r] % p
+
+print(cmb(6000, 4343, MOD))
